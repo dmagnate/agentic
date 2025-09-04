691b0474aa329f3a749f6efa67975b92484ef2d0
devdevesh <deveshsharma1989@gmail.com>
2025-09-04 22:48:55 +0100

    Update format operator

--- python_work/002_variables.py
+++ python_work/002_variables.py
@@ -4,6 +4,3 @@ Introduction to variables, Start with alphabets or underscore
 simple_string = "hello \'world'" ##string is series of character in quotes, use escape for quote within quotes
 print(simple_string)
 
-print(simple_string.upper()) ## change to uppercase
-print(simple_string.lower()) ## change to lowercase
-
--- python_work/003_objects.py
+++ python_work/003_objects.py
@@ -1,8 +1,20 @@
-print()
 '''
 Objects - 
 1. Every object is unique
 2. Every object have unique attributes
 3. Each object can have abstractions (classifications or class) based on attributes
 4. We can create/define objects according to abstractions or classes
+
+String is a type of class
+Methods/Behavior/Functions of string, example trim the string, change to lowercase, uppercase, etc.
+These methods can be used to create (print) different objects.
 '''
+
+simple_string = "Hello World" ##string is series of character in quotes, use escape for quote within quotes
+print(simple_string)
+print(simple_string.upper()) ## change to uppercase
+print(simple_string.lower()) ## change to lowercase
+
+question = "Name"
+query = input(f"What is your {question}?") ## Format operator help in taking input from previous string
+print(query.capitalize())
